ad6573203737b4df9ee84ccf197749812a488c4e
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-04-08 13:46:25 +0530

    initial commit

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
+// import millify from 'millify';
+import { Collapse, Row, Col, Typography } from 'antd';
+// import HTMLReactParser from 'html-react-parser';
 
 import { useGetExchangesQuery } from '../services/cryptoApi';
 import Loader from './Loader';
