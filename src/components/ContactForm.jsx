774db69cf2d748a17c8ac256d4aa94bf402e7e1e
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-04-25 01:32:00 +0530

    initial commit

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,16 @@
 import css from "../styles/ContactForm.module.css"
+import { MDBIcon } from 'mdb-react-ui-kit';
+import ReactCountryFlag from "react-country-flag"
 export default function ContactForm()
 {
-
     return(
         <div className={css.main}>
         <div className={css.container}>
-            <h2 className={css.head}>Contact Form</h2>
+            <h2 className={css.head}>
+                <ReactCountryFlag countryCode="IN" svg style={{borderRadius:'100%'}} />
+                &nbsp;Contact Form&nbsp;
+                <ReactCountryFlag countryCode="IN" svg style={{borderRadius:'100%'}} />
+            </h2>
                     <form className={css.form} id="contactForm" action="https://formsubmit.co/nirajtiwari199731@gmail.com" method="POST">
                             <input className={css.contactFormField} type="hidden" name="_captcha" value="false"/>
                             <input type="hidden" name="_subject" value="Hey there, I have visited your Portfolio!"/>
